376297c8170d294c3718a88421ebbb3fce8eb868
mahender <bmahender1990@gmail.com>
2020-02-05 15:49:32 +0530

    consumer updates

--- spring-boot-kafka-consumer/src/main/java/com/mahe/springkafkaconsumer/config/KafkaConfiguration.java
+++ spring-boot-kafka-consumer/src/main/java/com/mahe/springkafkaconsumer/config/KafkaConfiguration.java
@@ -2,7 +2,6 @@ package com.mahe.springkafkaconsumer.config;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,26 +16,27 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 @EnableKafka
 @Configuration
 public class KafkaConfiguration {
-	
-	@Value("${kafka.bootstrap-server}")
-	private String server;
-	
-	@Bean
-	public ConsumerFactory<String, Object> consumerFactory(){
-		Map<String,Object> config = new HashMap<>();
-		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
-		config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
-		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		
-		return new DefaultKafkaConsumerFactory<String, Object>(config);
-	}
-		
-	@Bean
-	public ConcurrentKafkaListenerContainerFactory<String,Object> kafkaListenerContainerFactory() {
-		ConcurrentKafkaListenerContainerFactory<String,Object> factory= new ConcurrentKafkaListenerContainerFactory<String,Object>(); 
-		factory.setConsumerFactory(consumerFactory());
-		return factory;
-	}
-	
+
+  @Value("${kafka.bootstrap-server}")
+  private String server;
+
+  @Bean
+  public ConsumerFactory<String, Object> consumerFactory() {
+    Map<String, Object> config = new HashMap<>();
+    config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
+    config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+    config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+    return new DefaultKafkaConsumerFactory<>(config);
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+    ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(consumerFactory());
+    return factory;
+  }
+
 }
--- spring-boot-kafka-consumer/src/main/java/com/mahe/springkafkaconsumer/listener/KafkaConsumer.java
+++ spring-boot-kafka-consumer/src/main/java/com/mahe/springkafkaconsumer/listener/KafkaConsumer.java
@@ -4,12 +4,8 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mahe.springkafkaconsumer.model.AuditLog;
 import com.mahe.springkafkaconsumer.model.Customer;
@@ -19,33 +15,33 @@ import com.mahe.springkafkaconsumer.service.ConsumerService;
 
 @Service
 public class KafkaConsumer {
-	private static final Logger LOG = LoggerFactory.getLogger(KafkaConsumer.class);
-	
-	@Autowired(required=true)
-	private ConsumerService service;
-	
-	@KafkaListener(topics="KafkaDemo",groupId="group_id")
-	public void consume(ConsumerRecord message) {
-			
-		LOG.info("consume data='{}' to topic='{}'", message.value(), message.topic());
-		String customerStr=(String) message.value();
+  private static final Logger LOG = LoggerFactory.getLogger(KafkaConsumer.class);
 
-        ObjectMapper objectMapper = new ObjectMapper();        
-        try {
-			Customer customer = objectMapper.readValue(customerStr, Customer.class);			
-			
-			service.addCustomer(customer);
-			
-			AuditLog audit =new AuditLog();			
-			audit.setCustomerNumber(customer.getCustomerNumber());
-			audit.setPayload(customerStr);
-			service.addAudit(audit);
-			
-		}  catch (Exception e) {
-			ErrorLog error= new ErrorLog(e.getLocalizedMessage(), e.getMessage(), customerStr);
-			service.addError(error);
-		}
-		
-	}
+  @Autowired(required = true)
+  private ConsumerService service;
+
+  @KafkaListener(topics = "KafkaDemo", groupId = "group_id")
+  public void consume(ConsumerRecord message) {
+
+    LOG.info("consume data" + message.value());
+    String customerStr = (String) message.value();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      Customer customer = objectMapper.readValue(customerStr, Customer.class);
+
+      service.addCustomer(customer);
+
+      AuditLog audit = new AuditLog();
+      audit.setCustomerNumber(customer.getCustomerNumber());
+      audit.setPayload(customerStr);
+      service.addAudit(audit);
+
+    } catch (Exception e) {
+      ErrorLog error = new ErrorLog(e.getLocalizedMessage(), e.getMessage(), customerStr);
+      service.addError(error);
+    }
+
+  }
 
 }
