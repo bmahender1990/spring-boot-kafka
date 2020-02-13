3a525e09b6059ba73f5513ab08c64ab8fdb20775
mahender <bmahender1990@gmail.com>
2020-02-13 19:04:57 +0530

    renamed packages

--- cloudkafka-producer/src/main/java/com/mahe/springkafka/customer/publisher/service/KafkaProducerService.java
+++
@@ -1,8 +0,0 @@
-package com.mahe.springkafka.customer.publisher.service;
-
-import com.mahe.springkafka.customer.publisher.model.Customer;
-
-public interface KafkaProducerService {
-
-  void send(Customer customer);
-}
